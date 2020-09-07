7459a101d3980a1d877c1aacbacfa6fcf6346512
plvigarg <pallavi312k@gmail.com>
2020-09-08 00:21:58 +0530

    commit 3

--- puppycompanyblog/core/views.py
+++ puppycompanyblog/core/views.py
@@ -1,2 +1,12 @@
 from flask import render_template, request, Blueprint
-code = Blueprint('core', __name__)
+core = Blueprint('core', __name__)
+
+
+@core.route('/')
+def index():
+    return render_template('index.html')
+
+
+@core.route('/info')
+def info():
+    return render_template('info.html')
