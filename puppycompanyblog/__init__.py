976cec5dbd2ba5714cf9f02b92ec16b667ae9ab4
plvigarg <pallavi312k@gmail.com>
2020-09-09 23:20:08 +0530

    "models"

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -1,9 +1,33 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import migrate
+import os
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+###########################
+# DATABASE
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'data.sqlite')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+##########################
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+
+###########################
 
 
 from puppycompanyblog.core.views import core
 from puppycompanyblog.error_pages.handlers import error_pages
+
 app.register_blueprint(core)
 app.register_blueprint(error_pages)
