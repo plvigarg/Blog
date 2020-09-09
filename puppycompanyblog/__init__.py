37537fadb0fa1e9cf4b02ff0ac99d92e9d8cdeea
plvigarg <pallavi312k@gmail.com>
2020-09-09 19:52:31 +0530

    "error pages

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -4,4 +4,6 @@ app = Flask(__name__)
 
 
 from puppycompanyblog.core.views import core
+from puppycompanyblog.error_pages.handlers import error_pages
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
